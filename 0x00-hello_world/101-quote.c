1c9b0418682b42e0e1b343d47e66f37382f0b901
theadnanerfan <theadnanerfan@gmail.com>
2023-02-16 09:49:56 -0800

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("this will produce a std error));
+sizeof("this will produce a std error"));
 return (1);
 }
