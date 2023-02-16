3abc4b5b0c218812721f3ed0b9a73bd7907337d7
theadnanerfan <theadnanerfan@gmail.com>
2023-02-16 08:37:03 -0800

    push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints string
+ * Return: 0 when succesfull
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
