42e6181b5ce55edcc86c01e1cd1ac179c6fbc899
theadnanerfan <theadnanerfan@gmail.com>
2023-02-16 09:47:47 -0800

    push

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - std err
+ *
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("this will produce a std error));
+return (1);
+}
