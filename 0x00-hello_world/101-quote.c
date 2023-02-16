c94f39709d762a827ffd30ba791e89de24a36ab1
theadnanerfan <theadnanerfan@gmail.com>
2023-02-16 09:54:25 -0800

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - std err
+ * main - writ and stderror
  *
  * Return: 1
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("this will produce a std error"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
