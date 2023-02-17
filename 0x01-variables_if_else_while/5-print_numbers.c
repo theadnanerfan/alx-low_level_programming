307d2267dd508f3d40fc147fa82250b25d5674b8
theadnanerfan <theadnanerfan@gmail.com>
2023-02-17 06:27:11 -0800

    push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers less than 10
+ * return: always 0
+ */
+
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers less than 10 with 2 putchars
+ * Return: 0
+ */
+
+int main(void)
+{
+int n = 48;
+while (n < 10)
+{
+putchar(n);
+n += 1;
+}
+putchar('\n');
+return (0);
+}
