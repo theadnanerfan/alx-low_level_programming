43af53bc5b9870c3f9a9754dc76afde6fafbe197
theadnanerfan <theadnanerfan@gmail.com>
2023-02-21 13:49:58 -0800

    Push

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,11 @@
  */
 int print_last_digit(int x)
 {
-	if (x < 0)
-	{
-	x *= -1;
 	x %= 10;
-	}
-	else
+	if (x< 0)
 	{
-	x %= 10;
+		x = x * -1;
 	}
-	_putchar(x);
+	_putchar('0' + x);
 	return (x);
 }
