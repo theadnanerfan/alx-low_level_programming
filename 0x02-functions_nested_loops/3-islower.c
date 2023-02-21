e47bdd66020ea1c207291dd26ee9e476e89e8b0e
theadnanerfan <theadnanerfan@gmail.com>
2023-02-21 13:08:13 -0800

    Push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
  * _islower - check if input is lowercase
- * @c: character to test.
- *
  * Return: 1 if true else 0 
  */
-int _islower(char char)
+int _islower(char ch)
 {
-	if (char >= 'a' && char <= 'z')
+	if (ch >= 'a' && ch <= 'z')
 	{
 		return (1);
 	}
