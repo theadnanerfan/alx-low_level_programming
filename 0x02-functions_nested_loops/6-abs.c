87c9c1a48179f7c911423aeafe05febb9a681290
theadnanerfan <theadnanerfan@gmail.com>
2023-02-21 13:33:53 -0800

    Push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - return absolute of a numb
+ * @x: number to get its abs
+ * Return: the absolute value.
+ */
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
+}
