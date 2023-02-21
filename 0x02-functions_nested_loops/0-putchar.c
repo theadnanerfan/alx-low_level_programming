7fa1812ff7d0cb2dc17aaa6f21d96fe46d829d57
theadnanerfan <theadnanerfan@gmail.com>
2023-02-21 12:40:54 -0800

    Push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _putchar: puts char
+ * main: puts _putchar
  * Return: 0
  */
 
@@ -15,5 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return 0;
+return (0);
 }
