d3dc0de7ec3b7704bebc021b97a3285a6e8164c9
theadnanerfan <theadnanerfan@gmail.com>
2023-02-21 13:01:06 -0800

    Push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - write 10 lower alphabets then line.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;	
+
+	for (i = 0; i < 10 ; i++)
+	{
+		for (x = 'a' ; x <= 'z' ; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
