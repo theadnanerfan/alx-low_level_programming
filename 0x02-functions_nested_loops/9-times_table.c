2121137b386906b26d475e5b1cadfb6eec8dec39
theadnanerfan <theadnanerfan@gmail.com>
2023-02-21 14:23:32 -0800

    Push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,34 +5,33 @@
  */
 void times_table(void)
 {
-    int row, col;
+int row, col;
 
-    for (row = 0; row < 10; row++)
-	{
-        _putchar('0');
-        for (col = 1; col < 10; col++)
-		{
-            int product = row * col;
-            _putchar(',');
-            _putchar(' ');
-
-            if (product < 10)
-			{
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-			else if (product < 100)
-			{
-                _putchar(product / 10 + '0');
-                _putchar(product % 10 + '0');
-            } 
-			else
-			{
-                _putchar(product / 100 + '0');
-                _putchar((product % 100) / 10 + '0');
-                _putchar(product % 10 + '0');
-            }
-        }
-        _putchar('\n');
-    }
+for (row = 0; row < 10; row++)
+{
+_putchar('0');
+for (col = 1; col < 10; col++)
+{
+int product = row * col;
+_putchar(',');
+_putchar(' ');
+if (product < 10)
+{
+_putchar(' ');
+_putchar(product + '0');
+}
+else if (product < 100)
+{
+_putchar(product / 10 + '0');
+_putchar(product % 10 + '0');
+} 
+else
+{
+_putchar(product / 100 + '0');
+_putchar((product % 100) / 10 + '0');
+_putchar(product % 10 + '0');
+}
+}
+_putchar('\n');
+}
 }
