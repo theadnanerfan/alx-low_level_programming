9c32bc6e30f383dada9a330a6751fa745ee1aa77
theadnanerfan <theadnanerfan@gmail.com>
2023-02-22 13:11:29 -0800

    Push

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * natural_sums_multiplies_under1024 - sums multiples of 3 and 5 under 1024
+ * Return: 0
+ */
+
+int main()
+{
+int sum = 0;
+int i = 0;
+for (i; i<1024; i++)
+{
+if (i % 3 == 0)
+{
+sum += i;
+continue;
+}
+else if (i % 5 == 0)
+{
+sum += i;
+continue;
+}
+}
+printf("%d", sum);
+printf("\n");
+return (0);
+}
