37c941e894ad9bd118a232d6de66928a541270c8
theadnanerfan <theadnanerfan@gmail.com>
2023-02-23 14:23:25 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all nums but 2/4
+ * Return: all nums but 2/4 then new line
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i != 2) || (i != 4))
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
