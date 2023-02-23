e6ed3115f75a2d81e2a8d0c546f027d47d768ff8
theadnanerfan <theadnanerfan@gmail.com>
2023-02-23 15:13:48 -0800

    Update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
+/**
+ * main - prime numbs
+ * Return: 0
+ */
+
 int main(void)
 {
-    long int n = 612852475143;
-    long int largest_prime_factor = 2;
+long int n = 612852475143;
+long int largest_prime_factor = 2;
 
-    while (n != largest_prime_factor) {
-        if (n % largest_prime_factor == 0) {
-            n = n / largest_prime_factor;
-        } else {
-            largest_prime_factor++;
-        }
-    }
+while (n != largest_prime_factor)
+{
+if (n % largest_prime_factor == 0)
+{
+n = n / largest_prime_factor;
+}
+else
+{
+largest_prime_factor++;
+}
+}
 
-    printf("%ld\n", largest_prime_factor);
-    return 0;
+printf("%ld\n", largest_prime_factor);
+return (0);
 }
