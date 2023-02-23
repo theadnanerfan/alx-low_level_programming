a04393ccc944ed624e4a605972a9721008b6df2f
theadnanerfan <theadnanerfan@gmail.com>
2023-02-23 15:09:37 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long int n = 612852475143;
+    long int largest_prime_factor = 2;
+
+    while (n != largest_prime_factor) {
+        if (n % largest_prime_factor == 0) {
+            n = n / largest_prime_factor;
+        } else {
+            largest_prime_factor++;
+        }
+    }
+
+    printf("%ld\n", largest_prime_factor);
+    return 0;
+}
