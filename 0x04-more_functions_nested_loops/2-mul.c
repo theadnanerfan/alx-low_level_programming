b4be226446dd2eb68142356fa7bcff5447160ebf
theadnanerfan <theadnanerfan@gmail.com>
2023-02-23 14:02:55 -0800

    Update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 inputs
+ * @a: number 1
+ * @b: number 2
+ * Return: value of product
+ */
+int mul(int a, int b)
+{
+		return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints nums from 0 to 9
+ * Return: numbers and line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
