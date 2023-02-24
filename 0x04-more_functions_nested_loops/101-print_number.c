36107ae43c37242f46121e75cdd319a885a052cd
theadnanerfan <theadnanerfan@gmail.com>
2023-02-24 12:11:12 -0800

    Update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,20 +2,18 @@
 
 /**
  * print_number - prints numbs with putchar
- * @size: number
- * Return: 0
+ * @n: number
  */
 
 void print_number(int n)
 {
 int digit, temp;
 temp = n;
-do {
+while (temp != 0)
+{
 digit = temp % 10;
 temp /= 10;
 _putchar(digit + '0');
 }
-while (temp != 0);
 _putchar('\n');
-return 0;
 }
