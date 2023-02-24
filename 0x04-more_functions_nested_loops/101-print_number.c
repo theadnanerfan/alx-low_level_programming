b5720deb9c84cab91b30be585ec230ce719b1b58
theadnanerfan <theadnanerfan@gmail.com>
2023-02-24 12:27:31 -0800

    Update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,7 +4,6 @@
  * print_number - prints a multi-digit integer using putchar
  * @n: the integer to print
  */
-
 void print_number(int n)
 {
 if (n < 0)
@@ -12,13 +11,9 @@ if (n < 0)
 _putchar('-');
 n = -n;
 }
-int divisor = 1;
-while (n / divisor >= 10)
-divisor *= 10;
-while (divisor > 0)
+if (n / 10)
 {
-int digit = (n / divisor) % 10;
-_putchar(digit + '0');
-divisor /= 10;
+print_number(n / 10);
 }
+_putchar(n % 10 + '0');
 }
