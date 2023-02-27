e7593d165b7ddb785493fb3138270125cdda8eaa
theadnanerfan <theadnanerfan@gmail.com>
2023-02-27 13:06:18 -0800

    Update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: points to string 
+ */
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		x++;
+	}
+	_putchar('\n');
+}
