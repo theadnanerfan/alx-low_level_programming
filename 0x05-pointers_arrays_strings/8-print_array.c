e1812e91ee1860ccc403afde70d18dee9264b2ae
theadnanerfan <theadnanerfan@gmail.com>
2023-02-27 14:01:25 -0800

    Update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints array values of given n indecies
+ * @a: array
+ * @n: number of values
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf(", %d", a[0]);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+	}
+		printf("\n");
+}
