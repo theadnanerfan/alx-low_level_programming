c1da56c72350a6f2e91771fc6542018bc3c5a751
theadnanerfan <theadnanerfan@gmail.com>
2023-03-06 09:04:43 -0800

    Update

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints sum of two diags of sq
+ * @a: matrix to print diagonals
+ * @size: matrix size
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int sum = 0;
+	unsigned int sum2 = 0;
+	
+	int x;
+
+	for (x = 0; x < size; x++)
+	{
+		sum += a[(size * x) + x];
+		sum2 += a[(size * (x + 1)) - (x + 1)];
+	}
+
+	printf("%d, %d\n", sum, sum2);
+}
