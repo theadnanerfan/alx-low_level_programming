121103168c5fe6f614c982480c2cc11ee7b0a3a0
theadnanerfan <theadnanerfan@gmail.com>
2023-03-06 08:05:05 -0800

    Update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - finds substring
+ * @haystack: string to look inside
+ * @needle: substring to find
+ * Return: pointer to the start of the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x, j;
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		for (y = 0; needle[y] != '\0'; y++)
+		{
+			if (haystack[x + y] != needle[y])
+				break;
+		}
+		if (!needle[y])
+			return (&haystack[x]);
+	}
+	return (NULL);
+}
