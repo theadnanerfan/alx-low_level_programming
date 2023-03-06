5b7c483cbed2f308b1b08abc1c709a71d8507231
theadnanerfan <theadnanerfan@gmail.com>
2023-03-06 07:59:02 -0800

    Update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strpbrk - finds the first occurrence in a string of any of a set of bytes
- * @str: the string to search for matches
- * @byte_set: the set of bytes to search for in the string
- * Return: a pointer to the byte in the string that matches one of the bytes in
- * the set, or NULL if no such byte is found
+ * *_strpbrk - searches a string
+ * @s: string to look for 
+ * @accept: string with the bytes needed
+ * Return: pointer to the byte
  */
-char *_strpbrk(char *str, char *byte_set)
+char *_strpbrk(char *s, char *accept)
 {
-	int idx_str, idx_byte_set;
+	int x, y;
 
-	for (idx_str = 0; *str != '\0'; idx_str++)
+	for (x = 0; *s != '\0'; x++)
 	{
-		for (idx_byte_set = 0; byte_set[idx_byte_set] != '\0'; idx_byte_set++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
-			if (*str == byte_set[idx_byte_set])
+			if (*s == accept[y])
 			{
-				return (str);
+				return (s);
 			}
 		}
-		str++;
+		s++;
 	}
 
 	return (NULL);
