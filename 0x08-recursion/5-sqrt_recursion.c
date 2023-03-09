a7c71e088488dce25bba834eb3258aa6598a49b2
theadnanerfan <theadnanerfan@gmail.com>
2023-03-09 11:19:43 -0800

    Update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
+ /**
+ * _sqrt_recursion - returns sqrt of n recursively
+ * @n: integer
+ * Return: 1 and -1 when fail
+ */
+ int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
+
 /**
  * _sqrt - helper function for sqrt
  * @x: number to sqrt
  * @y: iteratable by recursion to get value of sqrt
  * Return: 1 and -1 when fail
- */
-
- /**
- * _sqrt_recursion - returns sqrt of n recursively
- * @n: integer
- * Return: 1 and -1 when fail
- *
  */
 
 int _sqrt(int x, int y)
@@ -21,8 +24,4 @@ int _sqrt(int x, int y)
 	else if (y * y > x)
 		return (-1);
 	return (_sqrt(x, y + 1));
-}
-int _sqrt_recursion(int n)
-{
-	return (_sqrt(n, 1));
 }
