6ed3ae0087862fe9ce660a16ee9766baafa2ce43
theadnanerfan <theadnanerfan@gmail.com>
2023-03-10 03:16:04 -0800

    Update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - output name of program
+ * @argc: cmd args nums
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
