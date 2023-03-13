fe3bce8b0c60125cd3797a5b0e01269f7fd8a359
theadnanerfan <theadnanerfan@gmail.com>
2023-03-13 11:33:23 -0700

    Update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strstr2 - copies a string
+ * @str: string we want to copy
+ * Return: succes points to string copied , null otherwise
+ */
+char *_strstr2(char *str)
+{
+	char *str2;
+	unsigned int i = 0;
+	unsigned int l = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[l])
+		l++;
+
+	str2 = malloc(sizeof(char) * (l + 1));
+
+	if (str2 == NULL)
+		return (NULL);
+
+	while ((str2[i] = str[i]) != '\0')
+		i++;
+
+	return (str2);
+}
