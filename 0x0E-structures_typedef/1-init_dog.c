b40f2b93c2ca2b9ca4b49190e5d98c04c9a7c145
theadnanerfan <you@example.com>
2023-03-20 21:39:05 +0200

    xx

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,21 @@
-#include "dog.h"
+nclude "dog.h"
 #include <stdio.h>
 /**
- * init_dog - dog with elemts
- * @d: struct point
- * @name: char point
- * @age: float age
- * @owner: owner point
- *
- * Description: inits var type
- * Return: nothing
- */
+ *  * init_dog - dog with elemts
+ *   * @d: struct point
+ *    * @name: char point
+ *     * @age: float age
+ *      * @owner: owner point
+ *       *
+ *        * Description: inits var type
+ *         * Return: nothing
+ *          */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+		if (d != NULL)
+				{
+							d->name = name;
+									d->age = age;
+											d->owner = owner;
+												}
 }
